d537d48f65876dc493112a613d3d3a3b04d7143f
Steve Birtles <sbirtles@farnborough.ac.uk>
2017-10-31 12:02:41 +0000

    Initial commit

---
+++ src/BinaryTree.java
@@ -0,0 +1,44 @@
+public class BinaryTree
+{
+
+    private BinaryNode root;
+
+    public BinaryTree() {
+        root = null;
+    }
+
+    public BinaryNode getRoot() {
+        return root;
+    }
+
+    public void addNode(String value) {
+        if (root == null) {
+            root = new BinaryNode(value);
+        }
+        else
+        {
+            addNodeRecursively(root, value);
+        }
+    }
+
+    public void addNodeRecursively(BinaryNode node, String value)
+    {
+        if (value.compareTo(node.getValue()) < 0)
+        {
+            if (node.getLeftChild() == null)
+            {
+                node.setLeftChild(new BinaryNode(value));
+            }
+            else
+            {
+                addNodeRecursively(node.getLeftChild(), value);
+            }
+        }
+        else
+        {
+
+            /* FILL IN THIS GAP */
+
+        }
+    }
+}
